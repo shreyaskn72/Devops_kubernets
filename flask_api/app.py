4c649c8947263a03a9d1be2d22557c1959839e6f
shreyaskn72 <shreyaskn72@gmail.com>
2026-05-09 19:57:49 +0530

    hello world message change

--- flask_api/app.py
+++ flask_api/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return {"message": "Hello, World from Shreyas K N!"}
+    return {"message": "Good morning from Shreyas K N!"}
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
