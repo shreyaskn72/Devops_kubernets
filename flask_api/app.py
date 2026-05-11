b6ba043b06516be7cfd5d7c5552526b43405e399
shreyaskn72 <shreyaskn72@gmail.com>
2026-05-11 17:33:33 +0530

    application change

--- flask_api/app.py
+++ flask_api/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -6,5 +7,20 @@ app = Flask(__name__)
 def hello():
     return {"message": "Good morning from Shreyas K N. Great job!"}
 
+
+@app.route('/greeting')
+def greeting():
+    data = request.args
+    name = data.get('Name')
+    city = data.get('City')
+    if not name or not city:
+        return {
+            "Message": "Name and City fields are required"
+        }, 403
+    return {
+        "Greeting": f"Hello {name}",
+        "Message": f"How are things at {city}?",
+    }, 200
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5001)
